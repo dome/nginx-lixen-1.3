5007633c7cb27275328973d09cba455bcf02e72b
Dome C <dome@tel.co.th>
2012-12-13 12:19:46 +0700

    bump version

--- bundle/nginx-1.3.7/src/core/nginx.h
+++ bundle/nginx-1.3.7/src/core/nginx.h
@@ -11,7 +11,7 @@
 
 #define nginx_version      1003007
 #define NGINX_VERSION      "1.3.7"
-#define NGINX_VER          "ngx_lixen_app/" NGINX_VERSION ".1lixen1"
+#define NGINX_VER          "lixen_appserv/" NGINX_VERSION ".1lixen1"
 
 #define NGINX_VAR          "NGINX"
 #define NGX_OLDPID_EXT     ".oldbin"
